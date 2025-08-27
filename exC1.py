a4abca30ee337ba13dbb9f52c2274de527e3f310
VanDinh2912 <vandinh29125@gmail.com>
2025-08-27 23:44:14 +0700

    Nop bai

---
+++ exC1.py
@@ -0,0 +1,33 @@
+print("Hello, Python in VS Code!")  #test 
+
+print("Hello world") 
+print("Albert Einstein once said, 'A person who never made a mistake never tried anything new.'") 
+if 5 > 2: 
+    print("Five is greater than two!")
+# Mo rong cau lenh 
+total = 1 + \
+2 + \
+3
+print(total)    ; print("Hello, Python in VS Code!") 
+
+#Dieu kien if elese
+if True: print("True") 
+else: print("False")
+
+#This is a comment
+print("Hello, Python!")
+
+#Math
+import math_libs
+print(math_libs.add(4,5))
+
+#Module1
+import module1
+print(module1.question)
+print(module1.answer)
+
+#Module2
+import module2
+print(module2.question)
+print(module2.answer)
+
