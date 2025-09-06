bda56ca1dcf90769edd637c85e04121109f56f82
VanDinh2912 <vandinh29125@gmail.com>
2025-09-06 23:47:37 +0700

    Chuong 3

--- Chương 3/Bai10.py
+++ Chương 3/Bai10.py
@@ -5,7 +5,7 @@ if n < 1 or x < 0:
 else:
     s= 0 
     GT = 1 
-    for i in range(1,n+1):
+    for i in range(n+1):
         GT *= i
         s += (x**i)/GT
     print(f"Giá trị của dãy số là: {s}")
